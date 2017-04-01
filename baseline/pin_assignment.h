ff1da578cd8f01ae154521ecabd04c331ac9023e
exednaorez <zeroandexe@gmail.com>
2017-04-01 00:35:27 -0700

    Update pin_assignment.h

--- baseline/pin_assignment.h
+++ baseline/pin_assignment.h
@@ -9,19 +9,19 @@ AnalogIn battery(PA_3);
 Serial pc(PA_9, PA_10);
 
 // Motor
-Motor leftMotor(PC_8, PC_7);
-Motor rightMotor(PB_9, PB_8);
+Motor leftMotor(PC_8, PC_7); // PB7, PB6
+Motor rightMotor(PB_9, PB_8); // PB9, PB8
 
 // Encoders
-Encoder leftEncoder(PB_3, PB_4);
-Encoder rightEncoder(PB_7, PB_6);
+Encoder leftEncoder(PB_3, PB_4); //PC9, PC8
+Encoder rightEncoder(PB_7, PB_6); //PA1, PA0
 
 // IRSensors 
-IRSensor leftIR(PC_3, PC_5);
-IRSensor rightIR(PH_0, PA_4);
-IRSensor rightDiagonalIR(PH_1, PA_5);
-IRSensor rightFrontIR(PC_0, PA_6);
-IRSensor leftFrontIR(PC_1, PA_7);
-IRSensor leftDiagonalIR(PC_4, PC_2);
+IRSensor leftIR(PC_3, PC_5); //PC7,pc5
+IRSensor rightIR(PH_0, PA_4);//pb10,pa4
+IRSensor rightDiagonalIR(PH_1, PA_5);//pa8,pa5
+IRSensor rightFrontIR(PC_0, PA_6);//pb0,pa6
+IRSensor leftFrontIR(PC_1, PA_7);//pb1, pa7
+IRSensor leftDiagonalIR(PC_4, PC_2);//pc6,pc4
 
 #endif
